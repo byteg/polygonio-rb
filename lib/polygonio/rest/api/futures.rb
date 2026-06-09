426a4865270b1d57298c2a187f95f0615890d124
Denis Kuznetcov <denis@f7.ru>
2026-06-09 11:00:18 +0400

    fix

--- lib/polygonio/rest/api/futures.rb
+++ lib/polygonio/rest/api/futures.rb
@@ -64,8 +64,11 @@ module Polygonio
         end
       end
 
-      def all_products(limit: 100, next_url: nil)
+      def all_products(last_trade_date_gte: nil, limit: 100, next_url: nil)
         url = next_url.present? ? next_url : "/futures/v1/products?limit=#{limit}"
+        if last_trade_date_gte.present?
+          url += "&last_trade_date.gte=#{last_trade_date_gte}"
+        end
         res = client.request.get(url)
         AllProductsResponse[res.body]
       end
